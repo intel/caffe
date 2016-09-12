ae24caa4382bf453db78e8476d1d0a8c3394d278
hjchen2 <chenhoujiangcug@gmail.com>
2016-09-12 10:53:54 +0800

    Update relu_layer.cpp

--- src/caffe/layers/relu_layer.cpp
+++ src/caffe/layers/relu_layer.cpp
@@ -21,17 +21,6 @@ void ReLULayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       for (int i = 0; i < count; ++i) {
           top_data[i] *= (bottom_data[i] > 0);
       }
-     /* int C = bottom[0]->shape(1);
-      int NHW = count / C;
-
-      #pragma omp parallel for
-      for (int i = 0; i < C; ++i) {
-          Dtype* top_data_tmp = top_data + i * NHW;
-          const Dtype* bot_data_tmp = bottom_data + i * NHW;
-          for (int j = 0; j < NHW; ++j) {
-              top_data_tmp[i] *= (bot_data_tmp[i] > 0);
-          }
-      }*/
   }
   else {
       #pragma omp parallel for
@@ -51,15 +40,6 @@ void ReLULayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     const int count = bottom[0]->count();
     Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
-/*
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-    for (int i = 0; i < count; ++i) {
-      bottom_diff[i] = top_diff[i] * ((bottom_data[i] > 0)
-          + negative_slope * (bottom_data[i] <= 0));
-    }
-*/
     if (bottom[0] != top[0]) {
         caffe_copy(count, top_diff, bottom_diff);
     }
